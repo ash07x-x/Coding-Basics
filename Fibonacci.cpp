1398bc8b63e33da3f11eee031c58ee5d2ca061c0
ash07x-x <priyanshaarora15@gmail.com>
2026-05-13 19:41:50 +0530

    Create Fibonacci.cpp
    Taking input and printing Fibonacci sequence till that position

---
+++ Fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int a = 0;
+    int b = 1;
+    int next;
+    cout << "How many Fibonacci numbers? ";
+    cin >> n;
+    cout << "Fibonacci Sequence: ";
+    for(int i = 0; i < n; i++) {
+        cout << a << " ";
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    return 0;
+}
