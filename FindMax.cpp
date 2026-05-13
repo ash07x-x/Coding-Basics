402ea4403c2adb2e3f28c0f46d22fe5be3abf068
ash07x-x <priyanshaarora15@gmail.com>
2026-05-13 19:52:48 +0530

    Update FindMax.cpp

--- FindMax.cpp
+++ FindMax.cpp
@@ -3,22 +3,22 @@
 #include <ctime>
 using namespace std;
 
-int main() {
+int main(){
     srand(time(0));
     int arr[10];
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 10; i++){
         arr[i] = rand() % 100 + 1;
     }
-    cout << "Array: ";
-    for(int i = 0; i < 10; i++) {
-        cout << arr[i] << " ";
+    cout<<"Array: ";
+    for(int i = 0; i < 10; i++){
+        cout<<arr[i] << " ";
     }
     int max = arr[0];
-    for(int i = 1; i < 10; i++) {
-        if(arr[i] > max) {
+    for(int i = 1; i < 10; i++){
+        if(arr[i] > max){
             max = arr[i];
         }
     }
-    cout << "\nMaximum = " << max;
+    cout << "Maximum = " << max;
     return 0;
 }
